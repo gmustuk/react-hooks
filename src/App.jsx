de0436154ef8d8256f698daa024dc04371a3d107
Gurumustuk Khalsa <gmustuk@gmail.com>
2023-05-24 10:40:31 -0600

    finished deck of cards get

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+
+import 'bootstrap/dist/css/bootstrap.css'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+const [cards, setCards] = useState([])
+    const [cardId, setCardId] = useState("")
+
+    const initialEffect = () =>{
+    fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+        .then(response => response.json())
+        .then(json => {
+            setCardId(json.deck_id)
+        })
+    }
+
+  useEffect(initialEffect,[])
+
+   const secondaryEffect = () => {
+
+    if(cardId !== "") {
+      fetch(`https://deckofcardsapi.com/api/deck/${cardId}/draw/?count=52`)
+          .then(response => response.json())
+              .then(json => {
+                  setCards(json.cards)
+              })
+    }
+   }
+
+   useEffect(secondaryEffect, [cardId])
+
+    console.log(cards)
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <div className="row row-cols-1 row-cols-md-2 g-4">
+            {cards.map(card => <DisplayCard card={card} key={card.code} />)}
+        </div>
     </>
   )
 }
 
 export default App
+
+
+function DisplayCard(props) {
+   const {card} = props
+
+    return(
+        <>
+        <div className="col">
+            <div className="card">
+                <img src={card.image} className="card-img-top" alt={card.code} />
+                <div className="card-body">
+                    <h5 className="card-title">{card.value} {card.suit}</h5>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
